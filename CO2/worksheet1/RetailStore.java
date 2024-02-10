abc709f60f013e106ff9f2c9187eee4cfeb9105f
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2024-02-10 19:01:57 +0530

    Update RetailStore.java

--- CO2/worksheet1/RetailStore.java
+++ CO2/worksheet1/RetailStore.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 
 public class RetailStore {
-    double billAmount;
-
-    public RetailStore(double billAmount) {
-        this.billAmount = billAmount;
-    }
-
-   
-    public double calculateDiscount() {
+   double billAmount;
+   public RetailStore(double billAmount)
+   {
+        this.billAmount=billAmount;
+   }
+    public double calculateDiscount(double billAmount)
+    {
         double discountPercentage;
-
-        if (billAmount >= 1000) {
+        if (billAmount>=1000)
+        {
+            discountPercentage = 10;
+        }
+        else if(billAmount>=500 && billAmount<1000) {
             discountPercentage = 5;
-        } else if (billAmount >= 500 && billAmount < 1000) {
-            discountPercentage = 2;
-        } else if (billAmount > 0 && billAmount < 500) {
+        }
+        else if(billAmount>0 && billAmount<500) {
             discountPercentage = 1;
-        } else {
+        }
+        else{
             discountPercentage = 0;
         }
 
         return discountPercentage;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -33,9 +34,10 @@ public class RetailStore {
         RetailStore retailStore = new RetailStore(billAmount);
 
       
-        double discountPercentage = retailStore.calculateDiscount();
+        double discountPercentage = retailStore.calculateDiscount(billAmount);
         System.out.println("Discount Percentage: " + discountPercentage + "%");
-
+        double discountedBill = billAmount-((discountPercentage/100)*billAmount);
+        System.out.println("Discount Price: " + discountedBill + "%");
         scanner.close();
     }
 }
