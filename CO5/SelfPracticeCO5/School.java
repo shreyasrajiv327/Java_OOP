60de1586a3637d0c57731587c761e2525c48cebf
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2024-04-15 07:39:26 +0530

    update

--- CO5/SelfPracticeCO5/School.java
+++ CO5/SelfPracticeCO5/School.java
@@ -1,37 +1,37 @@
-class AgeExceptions extends Exception{
-      public AgeExceptions(String msg){
+class AgeException extends Exception{
+    AgeException(String msg)
+    {
         super(msg);
-      }
+    }
 }
+
 class Student{
     String Name;
     int age;
-
-    Student(String N,int a){
-        this.Name = N;
+    Student(String N,int a)
+    {
+        this.Name =N;
         this.age = a;
     }
-
-    public void agechecker() throws AgeExceptions{
-        if(age>21 || age<15){
-            throw new AgeExceptions("Age not withing the range");
-        }
-        else{
-            System.out.println("Age within the range");
-        }
+    public void checkAge() throws AgeException{
+          if(age>21 || age<15)
+          {
+             throw new AgeException("Invalid Age");
+          }
+          else{
+            System.out.println("Age is fine");
+          }
     }
 }
 public class School {
     public static void main(String[] args) {
-        
+        Student S1 = new Student("Shreyas",13);
+       try{
+        S1.checkAge();
+       }catch(AgeException e)
+       {
+         System.out.println(e.getMessage());
+       }
    
-    Student S1 = new Student("Shreyas",13);
-    try{
-        S1.agechecker();
-    }
-    catch(AgeExceptions e)
-    {
-        System.out.println(e.getMessage());
-    }
 }
 }
